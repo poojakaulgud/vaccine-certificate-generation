b669fe2f41940dc4a8c4c877c4dba3869c959f8b
Pooja Kaulgud <pooja.kaulgud@somaiya.edu>
2023-04-11 20:26:55 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
+import Web3 from 'web3'
 import './App.css';
 
 function App() {
+  // const [web3, setWeb3] = useState(null);
+  // const [accounts, setAccounts] = useState(null);
+  // const [contract, setContract] = useState(null);
+
+  // const web3 = await getWeb3();
+  
+  // const accounts = await web3.eth.getAccounts();
+  // const networkId = await web3.eth.net.getId();
+
+  // const deployedNetwork = Sanaaj.networks[networkId];
+  // const instance = new web3.eth.Contract(
+  //   Sanaaj.abi,
+  //   deployedNetwork && deployedNetwork.address,
+  // );
+  // setWeb3(web3);
+  // setAccounts(accounts);
+  // setContract(instance);
   return (
     <div className="App">
       <header className="App-header">
